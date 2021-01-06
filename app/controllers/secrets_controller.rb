9301696200812fa636805b0f74fa5f2ab9e59b79
joshsrai <joshsrai@gmail.com>
2021-01-06 17:02:43 -0500

    commit tree 58c8a47e81ce79f395f2a21d3fcc2f98a546ca1a

---
+++ app/controllers/secrets_controller.rb
@@ -0,0 +1,17 @@
+class SecretsController < ApplicationController
+    before_action :require_login
+
+    def show
+
+    end
+
+    private
+
+    def require_login
+      redirect_to login_path unless session.include? :name
+    end
+
+end
+    
+# Write a SecretsController using a before_action
+# to prevent any route from being accessed without logging in.
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'sessions#hello'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/show' => 'secrets#show'
+  get '/logout' => 'sessions#destroy'
 end
